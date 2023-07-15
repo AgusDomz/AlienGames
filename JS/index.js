4b4cc4426d1a44023804111233b9b21595655dab
Agustín Dominguez <131421006+AgusDomz@users.noreply.github.com>
2023-07-15 20:43:55 -0300

    :octocat:

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,39 @@
-'use strict';
+"use strict";
+let userAdmin = {email: "admin@admin.com", contraseña: "Admin1234"};
+let stateSesion = JSON.parse(sessionStorage.getItem("EstadoDeSesion")) || false;
+let user = JSON.parse(sessionStorage.getItem("userActivo")) || "";
+let linkAdmin = document.getElementById("linkAdmin");
+let userBtn = document.getElementById("userBtn");
+let exitBtn = document.getElementById("exitBtn");
+
+
+const closeSesion = () => {
+    if (stateSesion) {
+        stateSesion = false;
+        sessionStorage.setItem("EstadoDeSesion", JSON.stringify(stateSesion))
+        localStorage.removeItem("user")
+        window.location.replace("index.html")
+    } else {
+        window.location.reload();
+    }
+}
+
+if (stateSesion) {
+    if (user.email === userAdmin.email &&
+        user.contraseña === userAdmin.contraseña) {
+        linkAdmin.className = "nav-link btn btn-ligh"
+        exitBtn.className = "btn text-ligth"
+        userBtn.className = "btn-user btn"
+        userBtn.innerHTML = "Admin"
+        userBtn.removeAttribute("href");
+        exitBtn.addEventListener("click", closeSesion);
+    } else {
+        exitBtn.className = "btn text-ligth"
+        userBtn.className = "btn-user btn text-ligth"
+        userBtn.innerHTML = user.userNombreUsuario;
+        userBtn.removeAttribute("href")
+        exitBtn.addEventListener("click", closeSesion);
+    }
+}
+
+
